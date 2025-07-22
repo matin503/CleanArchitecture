ba349d75f9d2133f0cbd683e17e2bb19638c2d13
matin <matinamini503@gmail.com>
2025-07-22 20:57:31 +0330

    update

---
+++ CleanArchitecture.WebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace CleanArchitecture.WebApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCorse(IServiceCollection service) =>
+            service.AddCors(option =>
+            {
+                option.AddPolicy("corsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                );
+            });
+    }
+}
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -3,31 +3,34 @@ using CleanArchitecture.Application.Services;
 using CleanArchitecture.Domain.Interfaces;
 using CleanArchitecture.Infrastructure.Data;
 using CleanArchitecture.Infrastructure.Repositories;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+builder.Services.AddCors();
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Register dependencies And Use InMemory Database
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseInMemoryDatabase("CleanArchitectureDb"));
 
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCors("corsPolicy");
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
